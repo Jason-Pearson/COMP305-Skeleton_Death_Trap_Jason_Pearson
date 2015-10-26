dccf5720ac9e3d3119ca7f7a5cb25e6d603ef72c
Jason-Pearson <jason-pearson-1-0-7@hotmail.com>
2015-10-26 04:51:54 -0400

    Added Background, Score System and Game Over State, Menu and Game Over Scenes and their Controller Scripts, Audio, and DeathTrigger
    DeathTrigger will act as GameController as well - in terms of both Fall
    and ZeroHits End States, play and stop the right audio and transition to
    Game Over Scene. Still need to add Win State/Scene via reach goal post
    collider - if it could spawn at the last platform (clone) prefab. Also,
    my "made by" credits in the Menu Scene.

--- Assets/_Scripts/DeathTrigger.cs
+++ Assets/_Scripts/DeathTrigger.cs
@@ -2,21 +2,45 @@
 using System.Collections;
 
 public class DeathTrigger : MonoBehaviour {
+   
+    public PlayerController playerControllerScript; // reference for the Player Controller script of type PlayerController class
+    private AudioSource[] audiosources;
+    private AudioSource _playerFall;
+    private AudioSource _background;
 
 	// Use this for initialization
 	void Start () {
-	
+        this.audiosources = gameObject.GetComponents<AudioSource>();
+        this._playerFall = this.audiosources[0];
+        this._background = this.audiosources[1];
+
+        GameObject playerObject = GameObject.FindWithTag("Player"); //create reference for Player gameobject, and assign the variable via FindWithTag at start
+        if (playerObject != null) // if the playerObject gameObject-reference is not null - assigning the reference via FindWithTag at first frame -
+        {
+            playerControllerScript = playerObject.GetComponent<PlayerController>();// - set the PlayerController-reference (called playerControllerScript) to the <script component> of the Player gameobject (via the gameObject-reference) to have access the instance of the PlayerController script
+        }
+        if (playerObject == null) //for exception handling - to have the console debug the absense of a player controller script in order for this entire code, the code in the GameController to work
+        {
+            Debug.Log("Cannot find PlayerController script for Health End State in GameController");
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if (playerControllerScript.hits < 1)
+        {
+            this._background.Stop();
+            this._playerFall.Play();
+            Application.LoadLevel("GameOver");
+        }
 	}
     void OnTriggerEnter2D(Collider2D other) // detect collider of another object
     {
         if (other.gameObject.CompareTag("Player")) // if that object has th tag "Player" -
         {
-            Application.LoadLevel(Application.loadedLevel); // reload the current scene
+            this._background.Stop();
+            this._playerFall.Play();
+            Application.LoadLevel("GameOver");
         }
     }
 }
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 //VELOCITYRANGE PUBLIC CLASS - Max and Min Velocity for Player Shown in Inspector
 [System.Serializable]
@@ -22,11 +23,18 @@ public class PlayerController : MonoBehaviour {
 	public float jump = 500f;
 	public VelocityRange velocityRange = new VelocityRange (300f, 1000f); // reference of type VelocityRange, holds min and max velocity value
 
+    public Text scoreLabel; // reference for score label - to display score in the game scene
+    public int score = 0; // holds the score (starting at 0) to be added upon and updated to the score label
+
+    public int hits = 20; // holds the health for the Player - starting at 100
+    public Text hitCounter; // visual counter for the Player health via this label
+
 	//PRIVATE INSTANCE VARIABLES
 	private AudioSource[] _audioSources;
     private AudioSource _playerHit;
     private AudioSource _playerJump;
     private AudioSource _coinPickup;
+    private AudioSource _playerFall;
 
     //private PhysicsMaterial2D _material;
 	private Rigidbody2D _rigidbody2D;
@@ -45,10 +53,13 @@ public class PlayerController : MonoBehaviour {
         this._animator = gameObject.GetComponent<Animator>();
         //this._material = gameObject.GetComponent<BoxCollider2D>().sharedMaterial;
 
+        this._SetScore();
+        this._HitCount();
         this._audioSources = gameObject.GetComponents<AudioSource>();
         this._playerHit = this._audioSources[0];
         this._playerJump = this._audioSources[1];
         this._coinPickup = this._audioSources[2];
+        this._playerFall = this._audioSources[3];
     }
 
     void FixedUpdate()
@@ -126,14 +137,16 @@ public class PlayerController : MonoBehaviour {
     {
         if (otherCollider.gameObject.CompareTag("Coin"))
         {
-            //Add Score +100
+            this.score += 1000;
+            _SetScore();
             Debug.Log("Coin");
             this._coinPickup.Play();
             Destroy(otherCollider.gameObject);
         }
         if (otherCollider.gameObject.CompareTag("Bullet"))
         {
-            //Decrease Life
+            this.hits--;
+            _HitCount(); 
             Debug.Log("Hit");
             this._playerHit.Play();
             Destroy(otherCollider.gameObject);
@@ -145,12 +158,14 @@ public class PlayerController : MonoBehaviour {
         if (otherCollider.gameObject.CompareTag("Enemy"))
         {
             this._playerHit.Play();
+            //Enact Physics Material to Bounce off Enemy?
             /*if(this._isGrounded == false)
             {
                 this._material.bounciness = 0.5f;
             }*/
-            //Enact Physics Material to Bounce off Enemy?
-            //Life Decrease
+            //Amount of Hits Decreased
+            this.hits--;
+            _HitCount();
         }
     }
 
@@ -175,4 +190,32 @@ public class PlayerController : MonoBehaviour {
             this._transform.localScale = new Vector3(-1f, 1f, 1f);
         }
     }
+    public void _SetScore() // method to update to the current score upon killing enemies or picking up Gold and Silver coins
+    {
+        this.scoreLabel.text = "Score: " + score; // label equals this string statement - score is concatenated to string for display
+    }
+    public void _HitCount() // method to update to the current score upon killing enemies or picking up Gold and Silver coins
+    {
+        this.hitCounter.text = "Hit: " + hits; // label equals this string statement - score is concatenated to string for display
+    }
+    //End Game Method - called in Game Controller script - after player health = 0
+    /*public void _EndGameHealthState()
+    {
+        Destroy(gameObject); // destroy player gameobject
+
+        //Disable these Labels that are for Gameplay
+        this.scoreLabel.enabled = false;
+        this.enemyKilled.enabled = false;
+        this.healthCounter.enabled = false;
+        //Enable these labels for the Game Over screen
+        this.finalScoreLabel.enabled = true;
+        this.gameOverLabel.enabled = true;
+        this.finalKillCount.enabled = true;
+        this.restartLabel.enabled = true;
+        this.finalScoreLabel.text = "Score: " + this.score; // set final score to this label
+        this.finalKillCount.text = "Kills: " + this.killCount; // set final kill count to this label
+        this.restartLabel.text = "Press 'R' to Restart"; // set restart label to this string statement, to notify play how to restart
+
+    }*/
+
 }
